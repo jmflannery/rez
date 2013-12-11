4d65b52e41a90a58751c92c0f12ae216a5856ff7
Jack Flannery <jmflannery81@gmail.com>
2013-12-10 22:04:12 -0500

    add some address validations

--- app/models/rez/address.rb
+++ app/models/rez/address.rb
@@ -1,4 +1,9 @@
 module Rez
   class Address < ActiveRecord::Base
+    validates :building_number, presence: true
+    validates :street_name, presence: true
+    validates :city, presence: true
+    validates :state, presence: true
+    validates :zip_code, presence: true
   end
 end
--- test/models/rez/address_test.rb
+++ test/models/rez/address_test.rb
@@ -2,5 +2,29 @@ require 'test_helper'
 
 module Rez
   describe Address do
+
+    it "has a valid factory" do
+      FactoryGirl.build(:address).must_be :valid?
+    end
+
+    it "is invalid without a building_number" do
+      FactoryGirl.build(:address, building_number: nil).wont_be :valid?
+    end
+
+    it "is invalid without a street_name" do
+      FactoryGirl.build(:address, street_name: nil).wont_be :valid?
+    end
+
+    it "is invalid without a city" do
+      FactoryGirl.build(:address, city: nil).wont_be :valid?
+    end
+
+    it "is invalid without a state" do
+      FactoryGirl.build(:address, state: nil).wont_be :valid?
+    end
+
+    it "is invalid without a zip_code" do
+      FactoryGirl.build(:address, zip_code: nil).wont_be :valid?
+    end
   end
 end
