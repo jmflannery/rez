1ec9149036c9e7524224c41c1568b90b99fe289b
Jack Flannery <jmflannery81@gmail.com>
2013-12-13 10:51:00 -0500

    AddressController test

--- test/controllers/rez/addresses_controller_test.rb
+++ test/controllers/rez/addresses_controller_test.rb
@@ -7,20 +7,23 @@ module Rez
 
       let(:address_attrs) { FactoryGirl.attributes_for(:address) }
 
-      it "returns HTTP created 201" do
-        post :create, address: address_attrs, use_route: 'rez'
-        response.status.must_equal 201
-      end
-    
-      it "creates an address" do
-        assert_difference('Address.count', 1) do
-          post :create, address: address_attrs, use_route: 'rez'
-        end
-      end
+      describe "from an authenticated user" do
 
-      it "returns the address in json format" do
-        post :create, address: address_attrs, use_route: 'rez'
-        response.body.must_equal(AddressSerializer.new(assigns(:address)).to_json)
+        it "returns HTTP created 201" do
+          post :create, address: address_attrs, use_route: 'rez'
+          response.status.must_equal 201
+        end
+      
+        it "creates an address" do
+          assert_difference('Address.count', 1) do
+            post :create, address: address_attrs, use_route: 'rez'
+          end
+        end
+
+        it "returns the address in json format" do
+          post :create, address: address_attrs, use_route: 'rez'
+          response.body.must_equal(AddressSerializer.new(assigns(:address)).to_json)
+        end
       end
     end
 
