9c14df50d443150b18ed8b359ea6402961900605
Jack Flannery <jmflannery81@gmail.com>
2013-12-26 21:31:51 -0500

    add destroy route for resumes resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rez::Engine.routes.draw do
   resources :profiles, only: [:create, :index, :show, :update, :destroy]
   resources :addresses, only: [:create, :index, :show, :update, :destroy]
-  resources :resumes, only: [:create, :index, :show]
+  resources :resumes, only: [:create, :index, :show, :destroy]
 end
