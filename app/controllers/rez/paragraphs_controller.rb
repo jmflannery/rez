dd2f6356b516be0673878c9bbbfb79a02e0c7a7d
Jack Flannery <jmflannery81@gmail.com>
2014-01-07 13:54:56 -0500

    add ParagraphsController#show

--- app/controllers/rez/paragraphs_controller.rb
+++ app/controllers/rez/paragraphs_controller.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 module Rez
   class ParagraphsController < ApplicationController
 
-    before_action :toke, only: [:create, :index]
+    before_action :toke, only: [:create, :index, :show]
+    before_action :set_paragraph, only: [:show]
 
     def create
       paragraph = Paragraph.create(paragraph_params)
@@ -14,7 +15,16 @@ module Rez
       render json: Paragraph.all
     end
 
+    def show
+      render json: @paragraph
+    end
+
     private
+
+    def set_paragraph
+      @paragraph = Paragraph.find_by(id: params[:id])
+      head :not_found unless @paragraph
+    end
 
     def paragraph_params
       params.require(:paragraph).permit([:text, :rank])
--- test/controllers/rez/paragraphs_controller_test.rb
+++ test/controllers/rez/paragraphs_controller_test.rb
@@ -75,5 +75,49 @@ module Rez
         end
       end
     end
+
+    describe "GET show" do
+
+      let(:paragraph) { FactoryGirl.create(:paragraph) }
+
+      describe "with a valid Toke key in the header" do
+
+        let(:current_user) { FactoryGirl.create(:user) }
+        let(:token) { FactoryGirl.create(:token, user: current_user) }
+        before do request.headers['X-Toke-Key'] = token.key end
+
+        describe "given a valid Paragraph id" do
+
+          it "responds with 200 OK" do
+            get :show, id: paragraph, use_route: 'rez'
+            response.status.must_equal 200
+          end
+
+          it "responds with the requested Paragraph in JSON format" do
+            get :show, id: paragraph, use_route: 'rez'
+            response.body.must_equal(ParagraphSerializer.new(paragraph).to_json)
+          end
+        end
+
+        describe "given an invalid Paragraph id" do
+
+          it "responds with 404 Not Found" do
+            get :show, id: 'wrong', use_route: 'rez'
+            response.status.must_equal 404
+          end
+        end
+      end
+
+      describe "without a valid Toke key in the header" do
+
+        let(:token) { FactoryGirl.create(:token) }
+        before do request.headers['X-Toke-Key'] = token.key end
+
+        it "responds with 401 Unauthorized" do
+          get :show, id: paragraph, use_route: 'rez'
+          response.status.must_equal 401
+        end
+      end
+    end
   end
 end
