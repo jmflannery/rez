6d7dc49865964a0f77dba6c92baa0ea657999ad3
Jack Flannery <jmflannery81@gmail.com>
2014-01-07 11:34:30 -0500

    add routes for items and paragraphs

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rez::Engine.routes.draw do
   resources :profiles, only: [:create, :index, :show, :update, :destroy]
   resources :addresses, only: [:create, :index, :show, :update, :destroy]
   resources :resumes, only: [:create, :index, :show, :update, :destroy]
+  resources :items, only: [:create, :index, :show, :update, :destroy]
+  resources :paragraphs, only: [:create, :index, :show, :update, :destroy]
 end
