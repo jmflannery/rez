42034a505a8fb537ff0f51ca6c19285f979756ef
Jack Flannery <jmflannery81@gmail.com>
2014-01-27 21:41:17 -0500

    update the resume model test

--- test/models/rez/resume_test.rb
+++ test/models/rez/resume_test.rb
@@ -18,21 +18,15 @@ module Rez
       subject.item_ids.must_equal []
     end
 
-    it "can add item_ids to it's item_id array" do
-      subject.item_ids << 3 << 22 << 101
-      subject.item_ids.must_equal [3, 22, 101]
-    end
-
-    it "returns an array of all items in the item_id array" do
-      subject.item_ids << item1.id << item2.id
+    it "can add Items" do
       subject.items.must_equal [item1, item2]
+      subject.item_ids.must_equal [item1.id, item2.id]
     end
 
-    it "can add item objects" do
+    it "returns an Active Record Relation with all Items in the item_id array" do
       subject.add_item(item1)
       subject.add_item(item2)
       subject.items.must_equal [item1, item2]
-      subject.item_ids.must_equal [item1.id, item2.id]
     end
   end
 end
