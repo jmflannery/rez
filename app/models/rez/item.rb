a0f3742f2aa38121f65a8f9e8290c07bc1c8a502
Jack Flannery <jmflannery81@gmail.com>
2014-01-27 21:36:10 -0500

    save after updating the bullet_ids array

--- app/models/rez/item.rb
+++ app/models/rez/item.rb
@@ -15,7 +15,13 @@ module Rez
     def add_bullet(bullet)
       if bullet && bullet.point_type == 'bullet'
         bullet_ids << bullet.id
+        save_bullets_ids!
       end
+    end
+
+    def save_bullets_ids!
+      bullet_ids_will_change!
+      save
     end
   end
 end
