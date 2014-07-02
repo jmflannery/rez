1507b972c1fb31500b910a4fbe86e63dd4050e64
Jack Flannery <jmflannery81@gmail.com>
2014-07-02 18:21:58 -0400

    Add items to SectionSerializer.

--- app/serializers/rez/section_serializer.rb
+++ app/serializers/rez/section_serializer.rb
@@ -1,6 +1,8 @@
 module Rez
   class SectionSerializer < ActiveModel::Serializer
     attributes :id, :name, :heading, :subheading
+
+    has_many :items
   end
 end
 
--- test/serializers/rez/section_serializer_test.rb
+++ test/serializers/rez/section_serializer_test.rb
@@ -5,10 +5,15 @@ module Rez
   describe SectionSerializer do
 
     let(:section) { FactoryGirl.create(:section) }
+    let(:item) { FactoryGirl.create(:item) }
+
+    before do
+      section.add_item(item)
+    end
 
     it 'serializes a Section to JSON' do
       serialized = SectionSerializer.new(section).to_json
-      serialized.must_equal %Q({"section":{"id":#{section.id},"name":"#{section.name}","heading":"#{section.heading}","subheading":"#{section.subheading}"}})
+      serialized.must_equal %Q({"section":{"id":#{section.id},"name":"#{section.name}","heading":"#{section.heading}","subheading":"#{section.subheading}","items":[{"id":#{item.id},"name":"#{item.name}","title":"#{item.title}","heading":"#{item.heading}","bullets":[],"paragraphs":[]}]}})
     end
   end
 end
