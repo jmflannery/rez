12c4833f95938f54a25e412d6540823623166352
Jack Flannery <jmflannery81@gmail.com>
2014-07-02 11:30:09 -0400

    Add has many Items association to Section.

--- app/models/rez/section.rb
+++ app/models/rez/section.rb
@@ -1,4 +1,26 @@
 module Rez
   class Section < ActiveRecord::Base
+
+    def items
+      Item.where(id: item_ids)
+    end
+
+    def add_item(item)
+      if valid_item? item
+        item_ids << item.id
+        save_item_ids!
+      end
+    end
+
+    private
+
+    def valid_item?(item)
+      item && !item.new_record? && item.id > 0
+    end
+
+    def save_item_ids!
+      item_ids_will_change!
+      save
+    end
   end
 end
--- test/models/rez/section_test.rb
+++ test/models/rez/section_test.rb
@@ -18,5 +18,21 @@ module Rez
     it 'has a valid Factory' do
       FactoryGirl.build(:section).must_be :valid?
     end
+
+    describe 'Items' do
+
+      let(:item) { FactoryGirl.create(:item) }
+      let(:item2) { FactoryGirl.create(:item) }
+
+      it 'initialy has no items' do
+        subject.items.must_be_empty
+      end
+
+      it 'can add items to its Item list' do
+        subject.add_item(item)
+        subject.add_item(item2)
+        subject.items.must_equal [item, item2]
+      end
+    end
   end
 end
