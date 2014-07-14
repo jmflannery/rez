10bfea72952d95d6c77a1f25e5b24c315bc209ac
Jack Flannery <jmflannery81@gmail.com>
2014-07-14 10:53:10 -0400

    Add update and destroy actions to SectionsController.

--- app/controllers/rez/sections_controller.rb
+++ app/controllers/rez/sections_controller.rb
@@ -1,10 +1,11 @@
 module Rez
   class SectionsController < ApplicationController
 
-    before_action :toke, only: [:create]
+    before_action :toke, only: [:create, :update, :destroy]
     before_action :set_resume, only: [:index]
     before_action :set_sections, only: [:index]
-    before_action :set_section, only: [:show]
+    before_action :set_section, only: [:show, :update, :destroy]
+    before_action :update_items, only: [:update]
 
     def create
       section = Section.new(section_params)
@@ -23,6 +24,16 @@ module Rez
       render json: @section
     end
 
+    def update
+      @section.update(section_params)
+      render json: @section
+    end
+
+    def destroy
+      @section.destroy
+      head :no_content
+    end
+
     private
 
     def set_sections
@@ -38,6 +49,17 @@ module Rez
         @section = Section.find_by(id: params[:id])
         head :not_found unless @section
       end
+    end
+
+    def update_items
+      return unless params[:section][:item_ids]
+      item_ids = params[:section][:item_ids].uniq.select do |item_id|
+        Item.exists? item_id
+      end
+      @section.item_ids = item_ids.map { |i| i.to_i }
+      @section.item_ids_will_change!
+      @section.save
+      params[:section].delete(:item_ids)
     end
 
     def set_resume
--- test/controllers/rez/sections_controller_test.rb
+++ test/controllers/rez/sections_controller_test.rb
@@ -133,5 +133,144 @@ module Rez
         end
       end
     end
+
+    describe "PUT update" do
+
+      let(:section) { FactoryGirl.create(:section) }
+      let(:update_attrs) {{ name: 'my section', heading: 'My Awesome Heading', subheading: 'My Sweet Sub-heading' }}
+
+      describe "with a valid Toke key in the header" do
+
+        let(:current_user) { FactoryGirl.create(:user) }
+        let(:token) { FactoryGirl.create(:token, user: current_user) }
+        before do request.headers['X-Toke-Key'] = token.key end
+
+        describe "on successfull update" do
+
+          it "responds with 200 OK" do
+            put :update, id: section, section: update_attrs, use_route: 'rez'
+            response.status.must_equal 200
+          end
+
+          it "updates the section record" do
+            put :update, id: section, section: update_attrs, use_route: 'rez'
+            section.reload.heading.must_equal('My Awesome Heading')
+            section.subheading.must_equal('My Sweet Sub-heading')
+          end
+
+          it "returns the updated section in JSON format" do
+            put :update, id: section, section: update_attrs, use_route: 'rez'
+            response.body.must_equal(SectionSerializer.new(section.reload).to_json)
+          end
+
+          describe "updating the Section's Items" do
+
+            let(:item1) { FactoryGirl.create(:item) }
+            let(:item2) { FactoryGirl.create(:item) }
+            let(:update_attrs) {{
+              item_ids: [item1.id, item2.id],
+              name: 'New name',
+              heading: 'New heading',
+              subheading: 'New subheading'
+            }}
+
+            it "updates the Section's item_ids" do
+              put :update, id: section, section: update_attrs, use_route: 'rez'
+              section.reload.item_ids.must_equal [item1.id, item2.id]
+            end
+
+            it "does not update invalid item ids" do
+              update_attrs[:item_ids] << 1234
+              put :update, id: section, section: update_attrs, use_route: 'rez'
+              section.reload.item_ids.wont_include 1234
+            end
+
+            it 'does not insert duplicate items' do
+              update_attrs[:item_ids] << item1.id
+              put :update, id: section, section: update_attrs, use_route: 'rez'
+              section.reload.item_ids.must_equal [item1.id, item2.id]
+            end
+
+            describe 'when the section already has items(s)' do
+
+              before do
+                section.add_item item1
+                update_attrs[:item_ids].delete_at(0)
+              end
+
+              it 'items not given in item_ids are removed from the section' do
+                put :update, id: section, section: update_attrs, use_route: 'rez'
+                section.reload.item_ids.wont_include item1.id
+              end
+            end
+          end
+        end
+
+        describe "given an invalid section id" do
+
+          it "responds with 404 Not Found" do
+            put :update, id: 'nope', section: update_attrs, use_route: 'rez'
+            response.status.must_equal 404
+          end
+        end
+      end
+
+      describe "with an invalid Toke key in the header" do
+
+        let(:token) { FactoryGirl.create(:token) }
+        before do request.headers['X-Toke-Key'] = token.key end
+
+        it "responds with 401 Unauthorized" do
+          put :update, id: section, section: update_attrs, use_route: 'rez'
+          response.status.must_equal 401
+        end
+      end
+    end
+
+    describe "DELETE destroy" do
+
+      before do @section = FactoryGirl.create(:section) end
+
+      describe "with a valid Toke key in the header" do
+
+        let(:current_user) { FactoryGirl.create(:user) }
+        let(:token) { FactoryGirl.create(:token, user: current_user) }
+        before do request.headers['X-Toke-Key'] = token.key end
+
+        describe "given a valid section id" do
+
+          it "destroys the section" do
+            assert_difference('Section.count', -1) do
+              delete :destroy, id: @section, use_route: 'rez'
+            end
+          end
+
+          it "responds with 204 No Content" do
+            delete :destroy, id: @section, use_route: 'rez'
+            response.status.must_equal 204
+            response.body.must_equal ''
+          end
+        end
+
+        describe "given an invalid section id" do
+
+          it "responds with 404 Not Found" do
+            delete :destroy, id: 'wrong', use_route: 'rez'
+            response.status.must_equal 404
+          end
+        end
+      end
+
+      describe "with an invalid Toke key in the header" do
+
+        let(:token) { FactoryGirl.create(:token) }
+        before do request.headers['X-Toke-Key'] = token.key end
+
+        it "responds with 401 Unauthorized" do
+          delete :destroy, id: @section, use_route: 'rez'
+          response.status.must_equal 401
+        end
+      end
+    end
   end
 end
