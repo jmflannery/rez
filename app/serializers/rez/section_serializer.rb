a1c153f927b3e27207ed9a2af581b4272b05f08b
Jack Flannery <jmflannery81@gmail.com>
2016-03-24 22:35:05 -0400

    Remove sections

--- app/serializers/rez/section_serializer.rb
+++
@@ -1,8 +0,0 @@
-module Rez
-  class SectionSerializer < ActiveModel::Serializer
-    attributes :id, :name, :heading, :subheading
-
-    has_many :items
-  end
-end
-
--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,13 @@ Rez::Engine.routes.draw do
   resources :resumes, only: [:create, :index, :show, :update, :destroy] do
     resource :profile, only: [:show]
     resource :address, only: [:show]
-    resources :sections, only: [:index]
+    resources :items, only: [:index]
   end
 
   resources :profiles, only: [:create, :index, :show, :update, :destroy]
   resources :addresses, only: [:create, :index, :show, :update, :destroy]
-  resources :sections, only: [:create, :index, :show, :update, :destroy] do
-    resources :items, only: [:create, :index]
-  end
-
   resources :items, only: [:create, :index, :show, :update, :destroy] do
+    resources :items, only: [:create, :index]
     resources :points, only: [:create, :index]
   end
 
